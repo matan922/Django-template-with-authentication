14a4470003ae8fb66d94f502a744b07ba5993a22
matan922 <matanten@gmail.com>
2023-01-14 19:12:21 +0200

    first commit

---
+++ base/views.py
@@ -0,0 +1,88 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import Http404
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
+
+
+
+# /////////////////////// LOG IN /////////////////////////////////
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+ 
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+ 
+        return token
+ 
+ 
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+# /////////////////////////// END LOG IN ////////////////
+
+#/////////////////////////// REGISTER START ////////////
+@api_view(['POST'])
+def register(request):
+        user = User.objects.create_user(
+                    username=request.data['username'],
+                    email=request.data['email'],
+                    password=request.data['password']
+                )
+        user.is_active = True
+        user.is_staff = True
+        user.save()
+        return Response("new user born")
+
+
+#/////////////////////////// END REGISTER //////////////
+
+
+
+# @permission_classes([IsAuthenticated])
+class ProductList(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
